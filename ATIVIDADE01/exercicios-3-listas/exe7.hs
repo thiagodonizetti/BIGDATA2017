2bba502d613cbaa92b0b6bd4362732d1655ddaa3
thiagodonizetti <thiagods05@gmail.com>
2017-10-16 04:52:40 -0200

    lista 3 final

---
+++ ATIVIDADE01/exercicios-3-listas/exe7.hs
@@ -0,0 +1,10 @@
+collatz x
+ | even x =  fromInteger x`div`2
+ | otherwise = 3*x+1
+
+applyCollatz x
+ |x == 1 = []
+ |collatz x == 1 = [1]
+ |otherwise = collatz x : applyCollatz (collatz x)
+
+collatzLen x = length $ applyCollatz x
---
+++ ATIVIDADE01/exercicios-3-listas/exe8.hs
@@ -0,0 +1,13 @@
+collatz x
+ | even x =  fromInteger x`div`2
+ | otherwise = 3*x+1
+
+applyCollatz x
+ |x == 1 = []
+ |collatz x == 1 = [1]
+ |otherwise = collatz x : applyCollatz (collatz x)
+
+collatzLen x = length $ applyCollatz x
+
+
+maxColl  = maximum[collatzLen i | i<- [1..1000000]]
